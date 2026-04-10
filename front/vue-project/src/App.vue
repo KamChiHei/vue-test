7a7e8d6a21f2942554b74a7d17750e647ad96e47
kamchihei <2379765202@qq.com>
2026-04-10 20:44:19 +0800

    visit youtube

--- front/vue-project/src/App.vue
+++ front/vue-project/src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -15,9 +15,10 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
+  <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">Visit YouTube</a>
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
